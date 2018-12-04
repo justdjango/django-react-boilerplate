dd308a591794c61c394c24002c0c07ac802735d2
mattfreire <freire1996@gmail.com>
2018-12-04 23:01:13 +0200

    added Hoc

---
+++ src/hoc/hoc.js
@@ -0,0 +1,3 @@
+const Hoc = props => props.children;
+
+export default Hoc;
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import Hoc from "./hoc/hoc";
 
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 
 const BaseRouter = () => (
-  <div>
+  <Hoc>
     <Route exact path="/login/" component={Login} />
     <Route exact path="/signup/" component={Signup} />
-  </div>
+  </Hoc>
 );
 
 export default BaseRouter;
